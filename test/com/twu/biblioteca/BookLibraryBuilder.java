8d73614ffa1c545731611dbd9307f6cde873911b
Juhi Jariwala <juhijari@thoughtworks.com>
2015-03-10 10:05:16 +0530

    Removed empty class BibliotecaAppTest.java

--- test/com/twu/biblioteca/BookLibraryBuilder.java
+++ test/com/twu/biblioteca/BookLibraryBuilder.java
@@ -1,7 +1,6 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.Library.Book;
-import com.twu.biblioteca.Library.LibraryCollection;
 
 import java.util.ArrayList;
 
@@ -13,7 +12,6 @@ public class BookLibraryBuilder {
     }
 
     public ArrayList<Book> withArraylistWithBookName(String... bookNames) {
-        LibraryCollection<Book> bookLibraryCollection = new LibraryCollection<Book>();
 
         for (String bookName : bookNames) {
             books.add(new Book(bookName, "author", "date"));
